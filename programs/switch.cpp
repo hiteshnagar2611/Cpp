936f79cd914e03bdb63d1d0aabb2af3bacfefbaf
hiteshnagar2611 <74867491+hiteshnagar2611@users.noreply.github.com>
2022-10-23 20:15:06 +0530

    Create switch.cpp

---
+++ programs/switch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    cin>>age;
+    switch (age)
+    {
+    case 18:
+        cout<<"you are 18"<<endl;
+        break;
+    case 16:
+        cout<<"you are 18"<<endl;
+        break;
+    case 17:
+        cout<<"you are 18"<<endl;
+        break;
+    default:
+        cout<<"no special case"<<endl;
+        break;
+    
+    cout<<"hii hitesh";
+    }
+    return 0;
+}
